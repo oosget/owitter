8dafeb42f0fe60626309ee7df993dcade0dc5ae8
oosget <oosget30@gmail.com>
2022-05-21 14:13:19 +0900

    Add Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default () => <span>Auth</span>;
+const Auth = () => {
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const onChange = ({target: {name, value}}) => setForm({...form, [name]: value});
+
+    const onSubmit = ({preventDefault}) => {
+        preventDefault();
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    value={form.email}
+                    onChange={onChange}
+                    required
+                    />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={form.password}
+                    onChange={onChange}
+                    required
+                />
+                <input type="submit" value="로그인" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with GitHub</button>
+            </div>
+        </div>
+    );
+}
+export default Auth;
